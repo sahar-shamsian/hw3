eb6992aaa9c9a646c99e8e39033424baba1495ba
sahar-shamsian <sahar.shamsian@kellogg.northwestern.edu>
2022-02-22 22:02:20 +0000

    Final draft

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,21 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all
-      end
-    
-      def new
-        @post = Post.new
-      end
-    
-      def create
-        @post = Post.new(params["post"])
-        @post.save
-        redirect_to "/places/#{@post.place.id}"
-      end
+  def index
+      @posts = Post.all
+    end
+  
+    def new
+      @post = Post.new
+      @place = Place.find(params["place_id"])
+      @post.place_id = @place.id
+
+    end
+  
+    def create
+      @post = Post.new(params["post"])
+      @post.save
+      redirect_to "/places/#{@post.place_id}"
+    end
 end
+
+
+
